9d6a1a4e4388bb9f6d22ca46a8dd631aecb7aea1
harshpratik2411 <harshsrivastava2357@gmail.com>
2025-02-06 15:27:06 +0530

    setup completed

---
+++ Portfolio/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';  // Add this line
+import './index.css';
+
+function App() {
+  return (
+    <>
+      <div className="hello">
+        <h1 className='text-red-600 text-5xl'>hello</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
+
